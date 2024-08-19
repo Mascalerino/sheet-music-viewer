412652407c0271e7328964fd99640926e5bb03b3
Manuel Lorenzo Tallon <mlorenzotallon@gmail.com>
2024-08-19 21:52:13 +0200

    Added categories and filter sheet music

--- src/app/pdf-viewer/pdf-viewer.component.ts
+++ src/app/pdf-viewer/pdf-viewer.component.ts
@@ -1,29 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-import { PdfLoaderService } from '../services/pdf-loader.service';
+import { Component, OnInit } from "@angular/core";
+import { PdfLoaderService } from "../services/pdf-loader.service";
 
 interface Pdf {
   name: string;
   url: string;
 }
 
+interface Category {
+  name: string;
+  pdfs: Pdf[];
+}
+
 @Component({
-  selector: 'app-pdf-list',
-  templateUrl: './pdf-viewer.component.html',
+  selector: "app-pdf-list",
+  templateUrl: "./pdf-viewer.component.html",
 })
 export class PdfListComponent implements OnInit {
-  pdfs: Pdf[] = [];
-  filteredPdfs: Pdf[] = [];
+  categories: Category[] = [];
+  allCategories: Category[] = []; // Guardar la lista completa de categorías
   selectedPdf: Pdf | null = null;
-  searchTerm: string = '';
+  searchTerm: string = "";
 
   constructor(private pdfLoaderService: PdfLoaderService) {}
 
   ngOnInit(): void {
-    this.pdfLoaderService.getPdfList().subscribe((data) => {
-      this.pdfs = data;
-      this.sortPdfs(); // Ordena las partituras alfabéticamente
-      this.filteredPdfs = this.pdfs; // Inicializa la lista filtrada
+    this.pdfLoaderService.getPdfList().subscribe((lines) => {
+      this.parseCategories(lines);
+      this.allCategories = JSON.parse(JSON.stringify(this.categories)); // Guardar copia de la lista completa
     });
+  }
+
+  parseCategories(lines: string[]): void {
+    let currentCategory: Category | null = null;
+
+    lines.forEach((line) => {
+      if (line.startsWith("#")) {
+        if (currentCategory) {
+          this.categories.push(currentCategory);
+        }
+        currentCategory = { name: line.substring(1).trim(), pdfs: [] };
+      } else if (currentCategory && line) {
+        const [name, url] = line.split(",");
+        currentCategory.pdfs.push({ name: name.trim(), url: url.trim() });
+      }
+    });
+
+    if (currentCategory) {
+      this.categories.push(currentCategory);
+    }
+
+    this.sortPdfsInCategories();
+  }
+
+  sortPdfsInCategories(): void {
+    this.categories.forEach((category) => {
+      category.pdfs.sort((a, b) => a.name.localeCompare(b.name));
+    });
+  }
+
+  filterPdfs(): void {
+    const normalizedSearchTerm = this.normalizeString(this.searchTerm);
+
+    if (!normalizedSearchTerm) {
+      // Si el término de búsqueda está vacío, restaurar la lista original
+      this.categories = JSON.parse(JSON.stringify(this.allCategories));
+    } else {
+      // Aplicar filtro a la copia original de las categorías
+      this.categories = this.allCategories.map((category) => {
+        return {
+          ...category,
+          pdfs: category.pdfs.filter((pdf) =>
+            this.normalizeString(pdf.name).includes(normalizedSearchTerm)
+          ),
+        };
+      });
+    }
+  }
+
+  normalizeString(str: string): string {
+    return str
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, ""); // Elimina acentos y diacríticos
   }
 
   selectPdf(pdf: Pdf): void {
@@ -32,23 +90,5 @@ export class PdfListComponent implements OnInit {
 
   closePdfViewer(): void {
     this.selectedPdf = null;
-  }
-
-  filterPdfs(): void {
-    const normalizedSearchTerm = this.normalizeString(this.searchTerm);
-    this.filteredPdfs = this.pdfs.filter((pdf) =>
-      this.normalizeString(pdf.name).includes(normalizedSearchTerm)
-    );
-  }
-
-  normalizeString(str: string): string {
-    return str
-      .toLowerCase()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, ''); // Elimina acentos y diacríticos
-  }
-
-  sortPdfs(): void {
-    this.pdfs.sort((a, b) => a.name.localeCompare(b.name));
   }
 }
--- src/app/services/pdf-loader.service.ts
+++ src/app/services/pdf-loader.service.ts
@@ -1,32 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-interface PdfEntry {
-  name: string;
-  url: string;
-}
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class PdfLoaderService {
-  private txtFilePath = 'assets/pdf-list.txt';
+  private txtFilePath = "assets/pdf-list.txt";
 
   constructor(private http: HttpClient) {}
 
-  getPdfList(): Observable<PdfEntry[]> {
-    return this.http.get(this.txtFilePath, { responseType: 'text' }).pipe(
-      map((data) => {
-        return data
-          .split('\n')
-          .filter((line) => line.trim() !== '' && !line.startsWith('#')) // Ignora líneas vacías y comentarios
-          .map((line) => {
-            const [name, url] = line.split(',');
-            return { name: name.trim(), url: url.trim() };
-          });
-      })
+  getPdfList(): Observable<string[]> {
+    return this.http.get(this.txtFilePath, { responseType: "text" }).pipe(
+      map((data) =>
+        data
+          .split("\n")
+          .map((line) => line.trim())
+          .filter((line) => line !== "")
+      )
     );
   }
 }
